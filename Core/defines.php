e97bfa84fb2127781a9a25c81a5ce1c3e2433f76
Panayotis <panayotis.kalogirou@gmail.com>
2024-10-03 19:35:51 +0300

    added defines file

---
+++ Core/defines.php
@@ -0,0 +1,12 @@
+<?php
+define('ROOTPATH', __DIR__);
+define('BASE_URL', $_SERVER['SERVER_NAME']);
+define('ROOT_FOLDER', '');
+
+define ("MIN_CSS_FILE", "/assets/css/style.css");
+define ("MIN_JS_FILE", "/assets/js/script.js");
+
+define('RECAPTCHA_SITE_KEY', '');
+define('RECAPTCHA_SECRET_KEY', '');
+define('RECAPTCHA_PERCENT', '' );
+define('RECAPTCHA_ACTION', '' );
